47dd63f4351dd22b13817d21a595708788a50cee
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-04-04 00:25:57 -0600

    API implementation

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+
+
+
+const timeElement = document.getElementById('time');
+const dateElement = document.getElementById('date');
+const currentWeatherItemsElement = document.getElementById('current-weather-items');
+const timezone = document.getElementById('timezone');
+const countryElement = document.getElementById('country');
+const weatherForecastElement = document.getElementById('weather-forecast');
+const currentTempElement = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'e5fd451a1ca495c311a214ee0f63f6e8';
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeElement.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateElement.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+
+}, 1000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let {latitude, longitude } = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json())
+    })
+}
